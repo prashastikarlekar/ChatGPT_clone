ce56bdfc27c66069242745d56562eb0512606f8c
Prashasti Karlekar <prashastikarlekar@gmail.com>
2023-08-31 17:49:14 -0500

    formatted site structure and UI style.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+/** @format */
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div className='app'>
+			<section className='side-bar'>
+				<button>+ New Chat</button>
+				<ul className='history'>
+					<li>Item 1</li>
+				</ul>
+				<nav>
+					<p>Made by Prashasti</p>
+				</nav>
+			</section>
+			<section className='main'>
+				<h1>PrashastiGPT</h1>
+				<ul className='feed'></ul>
+				<div className='bottom-section'>
+					<div className='input-container'>
+						<input />
+						<div id='submit'>➢</div>
+					</div>
+					<p className='info'>
+						Free Research Preview. ChatGPT may produce inaccurate information
+						about people, places, or facts. ChatGPT August 3 Version
+					</p>
+				</div>
+			</section>
+		</div>
+	);
+};
 
 export default App;
