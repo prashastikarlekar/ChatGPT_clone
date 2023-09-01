18f40c5a0beefa881dff8d21af91f1e103558a1d
Prashasti Karlekar <prashastikarlekar@gmail.com>
2023-08-31 19:00:39 -0500

    New chat functionality and main body of app completed.

--- server.js
+++ server.js
@@ -3,13 +3,12 @@
 const PORT = 8000;
 const express = require("express");
 const cors = require("cors");
-
+require("dotenv").config();
 const app = express();
 app.use(express.json());
 app.use(cors());
 
-// const API_KEY = "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";
-const API_KEY = "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";
+const API_KEY = process.env.API_KEY;
 app.post("/completions", async (req, res) => {
 	const options = {
 		method: "POST",
@@ -19,7 +18,7 @@ app.post("/completions", async (req, res) => {
 		},
 		body: JSON.stringify({
 			model: "gpt-3.5-turbo",
-			messages: [{ role: "user", content: "How are you?" }],
+			messages: [{ role: "user", content: req.body.message }],
 			max_tokens: 100,
 		}),
 	};
--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 /** @format */
+import { useState, useEffect } from "react";
 
 const App = () => {
+	const [value, setValue] = useState(null);
+	const [message, setMessage] = useState(null);
+	const [previousChats, setPreviousChats] = useState([]);
+	const [currentTitle, setCurrentTitle] = useState(null);
+
+	const createNewChat = () => {
+		setMessage(null);
+		setValue("");
+		setCurrentTitle(null);
+	};
+	const handleClick = (uniqueTitle) => {
+		setCurrentTitle(uniqueTitle);
+		setMessage(null);
+		setValue("");
+	};
+
 	const getMessages = async () => {
 		const options = {
 			method: "POST",
 			body: JSON.stringify({
-				message: "Hello! How are you doing today?",
+				message: value,
 			}),
 			headers: {
 				"content-Type": "application/json",
@@ -17,28 +34,65 @@ const App = () => {
 				options
 			);
 			const data = await response.json();
-			console.log(data);
+			// console.log(data);
+			setMessage(data.choices[0].message);
 		} catch (error) {
 			console.error(error);
 		}
 	};
+	useEffect(() => {
+		console.log(currentTitle, value, message);
+		if (!currentTitle && value && message) {
+			setCurrentTitle(value);
+		}
+		if (currentTitle && value && message) {
+			setPreviousChats((prevChats) => [
+				...prevChats,
+				{ title: currentTitle, role: "user", content: value },
+				{
+					title: currentTitle,
+					role: message.role,
+					content: message.content,
+				},
+			]);
+		}
+	}, [message, currentTitle]);
+
+	const currentChat = previousChats.filter(
+		(previousChat) => previousChat.title === currentTitle
+	);
+
+	const uniqueTitles = Array.from(
+		new Set(previousChats.map((previousChat) => previousChat.title))
+	);
 	return (
 		<div className='app'>
 			<section className='side-bar'>
-				<button>+ New Chat</button>
+				<button onClick={createNewChat}>+ New Chat</button>
 				<ul className='history'>
-					<li>Item 1</li>
+					{uniqueTitles?.map((uniqueTitle, index) => (
+						<li key={index} onClick={() => handleClick(uniqueTitle)}>
+							{uniqueTitle}
+						</li>
+					))}
 				</ul>
 				<nav>
 					<p>Made by Prashasti</p>
 				</nav>
 			</section>
 			<section className='main'>
-				<h1>PrashastiGPT</h1>
-				<ul className='feed'></ul>
+				{!currentTitle && <h1>PrashastiGPT</h1>}
+				<ul className='feed'>
+					{currentChat.map((chatMessage, index) => (
+						<li key={index}>
+							<p className='role'>{chatMessage.role}</p>
+							<p>{chatMessage.content}</p>
+						</li>
+					))}
+				</ul>
 				<div className='bottom-section'>
 					<div className='input-container'>
-						<input />
+						<input value={value} onChange={(e) => setValue(e.target.value)} />
 						<div id='submit' onClick={getMessages}>
 							➢
 						</div>
